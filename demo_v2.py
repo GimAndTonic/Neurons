d8d3caad98ede76cb6bb815d8c248d6f23db8728
GimAndTonic <tim.gt@icloud.com>
2023-11-24 01:57:51 +0100

    All saved

--- demo_v2.py
+++ demo_v2.py
@@ -1,9 +1,12 @@
 # %%
 import random
+import time
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from IPython.display import Image
+from IPython.display import display, clear_output
+
 
 # %% [markdown]
 # # Something to learn
@@ -40,6 +43,7 @@ Y = np.array([0.0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0, 2.2, 2.4, 2
 # %%
 # configuration
 learning_rate = 0.01
+normalisation = 20
 
 ## preset/initials
 # initial weight
@@ -55,7 +59,6 @@ print("Initial Bias: [%s]" % (bias))
 # # Compute Functions
 
 # %%
-
 # Inner neuron
 def compute_neuron(X, W, bias) :
     return np.dot(X, W) + bias
@@ -93,6 +96,13 @@ weight_history = list()
 bias_history = list()
 loss_history   = list()
 
+# Set up the initial plot
+plt.ion()  # Turn on interactive mode
+fig, ax = plt.subplots()
+line1, = ax.plot(20*X, 20*Y, marker='o', color='blue', markersize=4, linewidth=2)
+line2, = ax.plot(20*X, 20*compute_neuron(X, weights, bias), marker='o', color='red', markersize=4, linewidth=2)
+plt.show()
+
 # Training loop
 for epoch in range(epochs):
     # Compute neuron output
@@ -108,16 +118,44 @@ for epoch in range(epochs):
     weights, bias = update_weights(weights, bias, dW, dB, learning_rate)
     # Optionally, print the loss at certain intervals
     if epoch % 10 == 0:
-        print(f"Epoch {epoch}, Loss: {loss}")
-        print(weights)
+        #print(f"Epoch {epoch}, Loss: {loss}")
+        #print(weights)
         weight_history.append(weights)
         bias_history.append(bias)
         loss_history.append(loss)
+
+        # Update the plot
+        line2.set_ydata(20*compute_neuron(X, weights, bias))
+        fig.canvas.draw()
+        fig.canvas.flush_events()
+        plt.pause(0.5)  # Pause to update the plot
+        clear_output(wait=True)  # Clear output to update the plot
+
+plt.ioff()  # Turn off interactive mode
+
+
+
 
 # Final weights and bias
 print("Final weights:", weights)
 print("Final bias:", bias)
 
+
+# %% [markdown]
+# # Show Graph
+
+# %%
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(20*X, 20*Y, marker='o', color='blue', markersize=4, linewidth=2)
+ax.plot(20*X, 20*compute_neuron(X, weights, bias), marker='o', color='red', markersize=4, linewidth=2)
+
+ax.set_title('Training Process ')
+ax.set_xlabel('Epoch')
+ax.set_ylabel('Loss')
+
+
+plt.show()
 
 # %%
 fig = plt.figure()
@@ -137,7 +175,7 @@ plt.show()
 
 # %%
 x = np.linspace(0, 1, 100)
-y = np.linspace(0, 1, 100)
+y = np.linspace(-1, 1, 100)
 x, y = np.meshgrid(x, y)
 
 z = list()
@@ -157,20 +195,51 @@ z = np.array(z)
 
 
 # %%
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+
+# Assuming x, y, z are defined properly
+
+fig = plt.figure(figsize=(10, 10))  # Adjust the figure size as needed
+
+# First subplot
+ax1 = fig.add_subplot(121, projection='3d')  # 2 rows, 1 column, 1st subplot
+ax1.plot_surface(x, y, z, cmap='rainbow')
+ax1.set_title('Optimisation Problem 1')
+ax1.set_xlabel('weight')
+ax1.set_ylabel('bias')
+ax1.set_zlabel('loss')
+
+# Second subplot
+ax2 = fig.add_subplot(122, projection='3d')  # 2 rows, 1 column, 2nd subplot
+ax2.plot_surface(x, y, np.log(z), cmap='rainbow')
+ax2.set_title('Optimisation Problem 2')
+ax2.set_xlabel('weight')
+ax2.set_ylabel('bias')
+ax2.set_zlabel('loss')
+
+# Display the plot
+plt.show()
+
+
+# %%
 
 fig = plt.figure(figsize=(10, 30))
 ax = fig.add_subplot(111, projection='3d')
-ax.plot_surface(x, y, np.log(z), cmap='rainbow')
-ax.plot(weight_history, bias_history, np.log(loss_history), marker='o')
+ax.plot_surface(x, y, np.log(z), cmap='rainbow', alpha=0.5)
+ax.plot(weight_history, bias_history, np.log(loss_history), marker='o', color='b', markersize=5, linewidth=2)
 
 
 ax.set_title('Optimisation Problem')
-ax.set_xlabel('w_1')
-ax.set_ylabel('w_2')
+ax.set_xlabel('weight')
+ax.set_ylabel('bias')
 ax.set_zlabel('loss')
 
-#ax.view_init(0, 0, 0)
 
 plt.show()
+
+# %%
+
 
 
